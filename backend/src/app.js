e8f2a95d65e102696dca06f4ca0626ddc483f4c3
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2025-03-10 01:13:23 +0100

    add env.example file

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/uploads", express.static("src/uploads"));
 
